4800214aa704ff40b7005c95fafceabdd7f27d43
abdullah-124 <mdabdullahsakib468@gmail.com>
2022-04-07 12:47:13 +0600

    cycliest

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ app.use(cors());
 app.use(express.json())
 
 const port = process.env.PORT || 4000;
+app.get("/", (req, res) => {
+  res.send("<h1>hello i am cyclist server</h1>");
+});
 
+app.listen(port, () => {
+  console.log(`I am running in port numebr ${port}`);
+});
 //MONGODB CONNECTION
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p6thn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
